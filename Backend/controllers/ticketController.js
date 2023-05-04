2e2b5eceec18da20fe82468f716725210740b39a
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-05-04 17:03:25 +0100

    Add ticketController and initial setu

--- Backend/controllers/ticketController.js
+++ Backend/controllers/ticketController.js
@@ -4,8 +4,20 @@ const user = require('../models/userModel')
 const ticket = require('../models/ticketModel')
 
 // @desc    Get user tickets
-// @route   /api/tickets
+// @route   GET /api/tickets
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'getTickets' })
 })
+
+// @desc    Create user tickets
+// @route   POST /api/tickets
+// @access  Private
+const createTicket = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'CreateTickets' })
+})
+
+module.exports = {
+  getTickets,
+  createTicket,
+}

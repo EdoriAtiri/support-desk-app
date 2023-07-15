55a171156baeb58d7898f6dbefbf47a708022890
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-07-15 12:47:02 +0100

    FE: Add initial ticket page display

--- frontend/src/app/pages/Ticket.jsx
+++ frontend/src/app/pages/Ticket.jsx
@@ -13,7 +13,7 @@ function Ticket() {
 
   const params = useParams()
   const dispatch = useDispatch()
-  const ticketId = useParams()
+  const { ticketId } = useParams()
 
   useEffect(() => {
     if (isError) {
@@ -21,9 +21,39 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId))
-  }, [dispatch, isError, message, ticketId])
+    // eslint-disable-next-line
+  }, [isError, message, ticketId])
 
-  return <div>Ticket</div>
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h3>Something Went Wrong</h3>
+  }
+
+  return (
+    <div className="ticket-page">
+      <header className="ticket-header">
+        <BackButton url="/tickets" />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-US')}
+        </h3>
+        <hr />
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+    </div>
+  )
 }
 
 export default Ticket

9026a6745ca97478dbd7589c9d0ef7a1091bfc07
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-07-15 13:04:51 +0100

    FE: Add reducer for createTicket

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -136,6 +136,14 @@ export const ticketSlice = createSlice({
         state.isError = true
         state.message = action.payload
       })
+      .addCase(closeTicket.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.tickets.map((ticket) =>
+          ticket._id === action.payload._id
+            ? (ticket.status = 'closed')
+            : ticket
+        )
+      })
   },
 })
 

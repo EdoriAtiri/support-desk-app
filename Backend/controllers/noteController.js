5c7dcff01f73cd6cb12699ef5a465c076ed80ba2
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-07-19 17:14:51 +0100

    BE: Add AddNote function to noteController

--- Backend/controllers/noteController.js
+++ Backend/controllers/noteController.js
@@ -23,11 +23,41 @@ const getNotes = asyncHandler(async (req, res) => {
     throw new Error('User not authorized')
   }
 
-  const notes = await Note.find({ ticket: req.params.ticketId })
+  const note = await Note.find({ ticket: req.params.ticketId })
+
+  res.status(200).json(note)
+})
+
+// @desc    Create ticket note
+// @route   POST /api/tickets/:ticketId/notes
+// @access  Private
+const addNotes = asyncHandler(async (req, res) => {
+  // Get user using the id in the jwt
+  const user = await User.findById(req.user.id)
+
+  if (!user) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const tickets = await Ticket.findById(req.params.ticketId)
+
+  if (tickets.user.toString() !== req.user.id) {
+    res.status(401)
+    throw new Error('User not authorized')
+  }
+
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    ticket: req.params.ticketId,
+    user: req.user.id,
+  })
 
   res.status(200).json(notes)
 })
 
 module.exports = {
   getNotes,
+  addNotes,
 }

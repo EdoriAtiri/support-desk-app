b17d02af55ff7cedb3c4fe22430c7cf8f64c1642
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2023-07-21 13:31:03 +0100

    FE: Create Modal and open/close function

--- frontend/src/app/pages/Ticket.jsx
+++ frontend/src/app/pages/Ticket.jsx
@@ -1,5 +1,7 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { toast } from 'react-toastify'
+import Modal from 'react-modal'
+import { FaPlus } from 'react-icons/fa'
 import { useSelector, useDispatch } from 'react-redux'
 import { getTicket, closeTicket } from '../../features/tickets/ticketSlice'
 import { getNotes, reset as noteReset } from '../../features/notes/noteSlice'
@@ -8,7 +10,24 @@ import BackButton from '../../components/BackButton'
 import Spinner from '../../components/Spinner'
 import NoteItem from '../../components/NoteItem.jsx'
 
+const customStyles = {
+  content: {
+    width: '600px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+  },
+}
+
+Modal.setAppElement('#root')
+
 function Ticket() {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const [noteText, setNoteText] = useState('')
   const { ticket, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.tickets
   )
@@ -37,6 +56,10 @@ function Ticket() {
     toast.success('Ticket Closed')
     navigate('/tickets')
   }
+
+  // Open/Close Modal
+  const openModal = () => setModalIsOpen(true)
+  const closeModal = () => setModalIsOpen(false)
 
   if (isLoading || notesIsLoading) {
     return <Spinner />
@@ -67,6 +90,25 @@ function Ticket() {
           <p>{ticket.description}</p>
         </div>
       </header>
+
+      {ticket.status !== 'closed' && (
+        <button onClick={openModal} className="btn">
+          <FaPlus />
+          Add Note
+        </button>
+      )}
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Add Note"
+      >
+        <h2>Add Note</h2>
+        <button className="btn-close" onClick={closeModal}>
+          X
+        </button>
+      </Modal>
 
       {notes.map((note) => (
         <NoteItem key={note._id} note={note} />
